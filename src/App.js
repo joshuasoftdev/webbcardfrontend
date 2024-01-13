401172976f5de83f7bc0170b72349b3125b4b665
joshuasoftdev <123251561+joshuasoftdev@users.noreply.github.com>
2024-01-13 03:08:17 -0800

    profiles responding

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [profile, setProfile] = useState(null);
+
+  useEffect(() => {
+    fetchProfiles();
+  }, []);
+
+  const fetchProfiles = async () => {
+    // fetch profile from server
+    const response = await axios.get("http://localhost:3000/profile");
+    // set profile state
+    console.log(response.data.profile);
+    setProfile(response.data.profile);
+  };
+  return <div className="App">fwerf</div>;
 }
 
 export default App;
